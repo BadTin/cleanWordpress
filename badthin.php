3d1828ba874a804758ac87c86e6b402b5def81c7
BadTin <22320229+BadTin@users.noreply.github.com>
2017-09-26 23:16:57 +0200

    Update badthin.php

--- badthin.php
+++ badthin.php
@@ -1,1 +1,15 @@
-// Start
+<?php
+/**
+* Plugin Name: My Plugin Name
+* Plugin URI: 
+* Description: 
+* Version: 1.0 
+* Author: BadTin
+* Author URI: 
+* License: GPL 3
+*/
+// Activate plugin
+
+// deaktivate plugin
+
+// Start Class
