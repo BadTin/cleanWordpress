73253795a7bed95e45e3dde0d3f94c7a7c0892f1
BadTin <22320229+BadTin@users.noreply.github.com>
2017-09-26 23:57:27 +0200

    Update badthin.php

--- badthin.php
+++ badthin.php
@@ -1,44 +1,76 @@
 <?php
 /**
-* Plugin Name: SSUP
-* Plugin URI: 
-* Description: 
-* Version: 1.0 
-* Author: BadTin
-* Author URI: 
-* License: GPL 3
-*/
-// ////////////////////////////////////////////////////////////////////////////////////////////
-// Anytime activate & deactivate your plugin!
-// ////////////////////////////////////////////////////////////////////////////////////////////
-function wis_core_activation() {
-} 
-register_activation_hook(__FILE__, 'wis_core_activation');
-function wis_core_deactivation() {
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://wasistseo.de
+ * @since             1.0.0
+ * @package           Badtin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       BadTin Toolbox for Wordpress
+ * Plugin URI:        https://wasistseo.de
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            BadTin
+ * Author URI:        https://wasistseo.de
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       badtin-
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
-register_deactivation_hook(__FILE__, 'wis_core_deactivation'); 
-// ////////////////////////////////////////////////////////////////////////////////////////////
-// Load all classes (Hooks) in the folder /classes with the extension _.php
-// ////////////////////////////////////////////////////////////////////////////////////////////
-foreach ( glob( plugin_dir_path( __FILE__ ) . "classes/*.php" ) as $hooks ) {
-    include_once $hooks;
-	}
-// ////////////////////////////////////////////////////////////////////////////////////////////
-// Start creating the plugin. We need a Menu and some other klickable stuff.
-// ////////////////////////////////////////////////////////////////////////////////////////////
-// Create in the Pluginfolder a link to Settingspage //
-function wis_plugin_action_links($links, $file) {
-static $this_plugin;
-if (!$this_plugin) {
-$this_plugin = plugin_basename(__FILE__);
-	}
-if ($file == $this_plugin) {
-$settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=wis-settings">CheckUP!</a>';
-array_unshift($links, $settings_link);
-	}
-return $links;
-	}
-add_filter('plugin_action_links', 'wis_plugin_action_links', 10, 2);
 
+define( 'PLUGIN_VERSION', '1.0.0' );
 
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-badtin--activator.php
+ */
+function activate_badtin_() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-badtin--activator.php';
+	Badtin_Activator::activate();
+}
 
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-badtin--deactivator.php
+ */
+function deactivate_badtin_() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-badtin--deactivator.php';
+	Badtin_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_badtin_' );
+register_deactivation_hook( __FILE__, 'deactivate_badtin_' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-badtin-.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_badtin_() {
+
+	$plugin = new Badtin();
+	$plugin->run();
+
+}
+run_badtin_();
